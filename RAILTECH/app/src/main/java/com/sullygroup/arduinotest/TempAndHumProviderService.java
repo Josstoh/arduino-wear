d232cdd1957c6422764a62191aad7eb180269a24
Jocelyn CARAMAN <josstoh@gmail.com>
2017-04-19 17:02:47 +0200

    - correction d'un problème qui empêchaient toutes les complciations de s'actualiser

--- RAILTECH/app/src/main/java/com/sullygroup/arduinotest/TempAndHumProviderService.java
+++ RAILTECH/app/src/main/java/com/sullygroup/arduinotest/TempAndHumProviderService.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -28,87 +32,104 @@ import com.google.android.gms.wearable.Wearable;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Provider service qui permet de mettre à jour la "Complication"(widget) sur la watchface.
+ * Provider service qui permet de mettre à jour les "Complications"(widget) sur la watchface.
  * Created by jocelyn.caraman on 20/03/2017.
  */
 
 public class TempAndHumProviderService extends ComplicationProviderService {
     private String TAG = "TAHProviderService";
     private GoogleApiClient googleApiClient;
+    private GoogleAPIHandler googleAPIHandler;
+    private Looper looper;
 
     @Override
     public void onComplicationUpdate(final int complicationId,final int dataType, final ComplicationManager complicationManager) {
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
-                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-                    @Override
-                    public void onConnected(@Nullable Bundle bundle) {
-                        PendingResult<DataItemBuffer> result = Wearable.DataApi.getDataItems(googleApiClient);
-                        result.setResultCallback(new ResultCallback<DataItemBuffer>() {
-                            @Override
-                            public void onResult(@NonNull DataItemBuffer dataItems) {
-                                int temp = -1;
-                                int hum = -1;
-                                String text = "";
-                                for (DataItem item : dataItems) {
-                                    if (item.getUri().getPath().equals("/stats/temp")) {
-                                        DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
-                                        temp = dataMap.getInt("value");
-                                        text += " temp. :" + temp + "C°";
-                                    }
-                                    else if(item.getUri().getPath().equals("/stats/hum")) {
-                                        DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
-                                        hum = dataMap.getInt("value");
-                                        text += "hum. :" + hum + "%";
-                                    }
-                                }
-                                ComplicationData complicationData;
-                                if(dataType == ComplicationData.TYPE_SHORT_TEXT){
-                                    // Type icone + valeur
-                                    SharedPreferences sharedPref = getSharedPreferences(
-                                            getString(R.string.preference_complications), Context.MODE_PRIVATE);
-                                    String type = sharedPref.getString(String.valueOf(complicationId), "none");
-                                    Log.d("test",type);
-                                    if(!type.equals("none")) {
-                                        Icon icon;
-                                        if(type.equals("temp")) {
-                                            text = temp + "C°";
-                                            icon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_thermometer);
-                                        }
-                                        else {
-                                            text = hum + "%";
-                                            icon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_humidity);
-                                        }
-                                        complicationData = new ComplicationData.Builder(ComplicationData.TYPE_SHORT_TEXT)
-                                                .setShortText(ComplicationText.plainText(text))
-                                                .setIcon(icon)
-                                                .setTapAction(PendingIntent.getActivity(getApplicationContext(),0,new Intent(getApplicationContext(),MainActivity.class),0))
-                                                .build();
-                                        complicationManager.updateComplicationData(complicationId, complicationData);
-                                    }
+        Log.d(TAG,"updating " + complicationId + "thread " + getMainLooper().toString());
+        HandlerThread myThread = new HandlerThread("BTHandlerThread");
+        myThread.start();
+        looper = myThread.getLooper();
+        googleAPIHandler = new GoogleAPIHandler(looper);
+        Message m = googleAPIHandler.obtainMessage(1,complicationId,dataType,complicationManager);
+        googleAPIHandler.sendMessage(m);
+    }
 
-                                }
-                                else {
-                                    // Type 2 valeurs (temp et hum)
-                                    complicationData = new ComplicationData.Builder(ComplicationData.TYPE_LONG_TEXT)
-                                            .setLongText(ComplicationText.plainText(text))
-                                            .setIcon(Icon.createWithResource(getApplicationContext(), R.drawable.ic_thermometer))
-                                            .setTapAction(PendingIntent.getActivity(getApplicationContext(),0,new Intent(getApplicationContext(),MainActivity.class),0))
-                                            .build();
-                                    complicationManager.updateComplicationData(complicationId, complicationData);
-                                }
-                                googleApiClient.disconnect();
-                                dataItems.release();
+    private class GoogleAPIHandler extends Handler {
+
+        GoogleAPIHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(final Message msg) {
+            Log.d(TAG,"handler début");
+            if(msg.what == 1){
+                googleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                    .addApi(Wearable.API)
+                    .build();
+                ConnectionResult connectionResult = googleApiClient.blockingConnect();
+                if(connectionResult.isSuccess()){
+                    Log.d(TAG, "onconnected " + msg.arg1+ "thread " + getMainLooper().toString());
+                    PendingResult<DataItemBuffer> result = Wearable.DataApi.getDataItems(googleApiClient);
+                    DataItemBuffer dataItems = result.await();
+                    Log.d(TAG, "onresult " + msg.arg1 + " status " + dataItems.getStatus()+ "thread " + getMainLooper().toString());
+                    int temp = -1;
+                    int hum = -1;
+                    String text = "";
+                    for (DataItem item : dataItems) {
+                        if (item.getUri().getPath().equals("/stats/temp")) {
+                            DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                            temp = dataMap.getInt("value");
+                            text += " temp. :" + temp + "C°";
+                        } else if (item.getUri().getPath().equals("/stats/hum")) {
+                            DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                            hum = dataMap.getInt("value");
+                            text += "hum. :" + hum + "%";
+                        }
+                    }
+                    ComplicationData complicationData;
+                    if (msg.arg2 == ComplicationData.TYPE_SHORT_TEXT) {
+                        Log.d(TAG, "short " + msg.arg1);
+                        // Type icone + valeur
+                        SharedPreferences sharedPref = getSharedPreferences(
+                                getString(R.string.preference_complications), Context.MODE_PRIVATE);
+                        String type = sharedPref.getString(String.valueOf(msg.arg1), "none");
+                        if (!type.equals("none")) {
+                            Icon icon;
+                            if (type.equals("temp")) {
+                                text = temp + "C°";
+                                icon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_thermometer);
+                            } else {
+                                text = hum + "%";
+                                icon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_humidity);
                             }
-                        });
-                    }
+                            complicationData = new ComplicationData.Builder(ComplicationData.TYPE_SHORT_TEXT)
+                                    .setShortText(ComplicationText.plainText(text))
+                                    .setIcon(icon)
+                                    .setTapAction(PendingIntent.getActivity(getApplicationContext(), 0, new Intent(getApplicationContext(), MainActivity.class), 0))
+                                    .build();
+                            ((ComplicationManager)msg.obj).updateComplicationData(msg.arg1, complicationData);
+                            Log.d(TAG, "updated " + msg.arg1 + " " + text);
+                        } else
+                            Log.d(TAG, "error type in preference file " + msg.arg1);
 
-                    @Override
-                    public void onConnectionSuspended(int i) {
-                        Log.d(TAG,"Error connection.");
+                    } else {
+                        Log.d(TAG, "long " + msg.arg1);
+                        // Type 2 valeurs (temp et hum)
+                        complicationData = new ComplicationData.Builder(ComplicationData.TYPE_LONG_TEXT)
+                                .setLongText(ComplicationText.plainText(text))
+                                .setIcon(Icon.createWithResource(getApplicationContext(), R.drawable.ic_thermometer))
+                                .setTapAction(PendingIntent.getActivity(getApplicationContext(), 0, new Intent(getApplicationContext(), MainActivity.class), 0))
+                                .build();
+                        ((ComplicationManager)msg.obj).updateComplicationData(msg.arg1, complicationData);
+                        Log.d(TAG, "updated " + msg.arg1 + " " + text);
                     }
-                })
-                .build();
-        googleApiClient.connect();
+                    dataItems.release();
+                } else {
+                    Log.d(TAG,"error " + msg.arg1);
+                }
+            }
+            Log.d(TAG,"handler fin");
+            super.handleMessage(msg);
+        }
     }
 }
